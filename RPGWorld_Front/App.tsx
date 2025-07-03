5279017df0ee3f6e0c2297fcae0959c844e49b2c
antonBlagodarnyy <antonbgi98@gmail.com>
2025-07-03 13:52:49 +0200

    FrontEnd Project first build

---
+++ RPGWorld_Front/App.tsx
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+
+import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View>
+        <Text>Hellow</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
